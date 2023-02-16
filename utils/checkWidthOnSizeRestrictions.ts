3a5194d9f2e2339b5c4ebd7297ac430d4241e28a
Vladimir-Maselskiy <65615017+Vladimir-Maselskiy@users.noreply.github.com>
2023-02-16 22:29:50 +0200

    fix: add if block for ts linter

--- utils/checkWidthOnSizeRestrictions.ts
+++ utils/checkWidthOnSizeRestrictions.ts
@@ -4,10 +4,11 @@ import sizeRestrictions from '../const';
 export const checkWidthOnSizeRestrictions = (
   currentSet: IFSet
 ): boolean => {
-  if (
-    +currentSet.width >= sizeRestrictions.maco.minWidth &&
-    +currentSet.width <= sizeRestrictions.maco.maxWidth
-  )
-    return true;
+  if (currentSet.width)
+    if (
+      +currentSet.width >= sizeRestrictions.maco.minWidth &&
+      +currentSet.width <= sizeRestrictions.maco.maxWidth
+    )
+      return true;
   return false;
 };
