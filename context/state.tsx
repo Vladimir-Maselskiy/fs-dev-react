8c83272657d7f6a6ed19e54cca7d0c2f164d3883
Vladimir-Maselskiy <65615017+Vladimir-Maselskiy@users.noreply.github.com>
2023-04-27 23:58:01 +0300

    feat: add UserContext

--- context/state.tsx
+++ context/state.tsx
@@ -1,4 +1,4 @@
-import { IArticleItem, IFSet } from '@/interfaces/interfaces';
+import { IArticleItem, IFSet, IUser } from '@/interfaces/interfaces';
 import { createContext, useContext, useState } from 'react';
 
 type Props = {
@@ -13,9 +13,14 @@ interface ITableContext {
   tableItems: IArticleItem[];
   setTableItems: React.Dispatch<React.SetStateAction<IArticleItem[]>>;
 }
+interface IUserContext {
+  user: IUser | null;
+  setUser: React.Dispatch<React.SetStateAction<IUser | null>>;
+}
 
 const FSetsContext = createContext<IFSetContext | null>(null);
 const TableItemsContext = createContext<ITableContext | null>(null);
+const UserContext = createContext<IUserContext | null>(null);
 
 export function AppWrapper({ children }: Props) {
   const [fSetsArray, setFSetsArray] = useState<IFSet[]>([]);
@@ -44,6 +49,18 @@ export function TableWrapper({ children }: Props) {
   );
 }
 
+export function UserWrapper({ children }: Props) {
+  const [user, setUser] = useState<IUser | null>(null);
+  let sharedState = {
+    user,
+    setUser,
+  };
+
+  return (
+    <UserContext.Provider value={sharedState}>{children}</UserContext.Provider>
+  );
+}
+
 export function useFSetsContext() {
   const context = useContext(FSetsContext);
   if (!context) {
@@ -57,7 +74,16 @@ export function useTableItemsContext() {
   const context = useContext(TableItemsContext);
   if (!context) {
     throw new Error(
-      'useFSetsContext has to be used within <useFSetsContext.Provider>'
+      'useTableContext has to be used within <useTableContext.Provider>'
+    );
+  }
+  return context;
+}
+export function useUserContext() {
+  const context = useContext(UserContext);
+  if (!context) {
+    throw new Error(
+      'useUserContext has to be used within <useUserContext.Provider>'
     );
   }
   return context;
