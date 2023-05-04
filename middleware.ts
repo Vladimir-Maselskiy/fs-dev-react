8e04dba6b8ce1e853d879df79faabbc378e66de0
Vladimir-Maselskiy <65615017+Vladimir-Maselskiy@users.noreply.github.com>
2023-05-04 17:59:06 +0300

    fix: try fix auth error  (401)

--- middleware.ts
+++ middleware.ts
@@ -9,18 +9,27 @@ import { NextApiRequest } from 'next';
 export function middleware(req: NextRequest) {
   const authHeader = new Headers(req.headers).get('authorization');
   console.log('authHeader', authHeader);
-  if (!authHeader) throw createError(401, 'Error user auth');
-  const token = authHeader.split(' ')[1];
-  if (!token) throw createError(401, 'Error user auth');
   //   try {
+  //     if (!authHeader) throw createError(401, 'Error user auth');
+  //     const token = authHeader.split(' ')[1];
+  //     if (!token) throw createError(401, 'Error user auth');
   //     getIsTokenValid(token, 'access');
-  //   } catch (error) {
-  //     throw createError(401, 'Error user auth');
-  //   }
+  //   } catch (error: any) {
+  //     //   throw createError(401, 'Error user auth');
+  return new NextResponse(
+    JSON.stringify({
+      success: false,
+      // message: error.cause,
+      status: 401,
+      headers: { 'content-type': 'application/json' },
+    })
+  );
   //   return NextResponse.redirect(new URL('/123', req.url));
+  //   console.log('middleware used2');
+  //   }
 }
 
 // See "Matching Paths" below to learn more
 export const config = {
-  matcher: '/api/users/:userID*',
+  matcher: '/api/users/:userId*',
 };
