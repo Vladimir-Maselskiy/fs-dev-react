417d87cc6cc79a86d9b00aa5a167723d3f76f610
Vladimir-Maselskiy <65615017+Vladimir-Maselskiy@users.noreply.github.com>
2023-10-06 13:08:24 +0300

    feat: try run cron 5

--- pages/api/cron.ts
+++ pages/api/cron.ts
@@ -5,6 +5,7 @@ import type { NextApiRequest, NextApiResponse } from 'next';
  */
 export default async function cron(req: NextApiRequest, res: NextApiResponse) {
   try {
+    res.json('{json...cron job runing...}');
     console.log('cron job runing...');
   } catch (error: any) {
     res.status(error.cause || 500).send({ error: error.message });
