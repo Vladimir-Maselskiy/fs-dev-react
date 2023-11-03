a076e48a1ef7d3491276aacf9e08c6c36964d2d5
Vladimir-Maselskiy <65615017+Vladimir-Maselskiy@users.noreply.github.com>
2023-11-03 19:28:56 +0200

    feat: add 211928 canvas base set

--- utils/canvas/getDafaultVerticalLockIcon.ts
+++ utils/canvas/getDafaultVerticalLockIcon.ts
@@ -4,6 +4,8 @@ import Icon211924HR from '../../public/articlesSVG/211924h-r.svg';
 import Icon211924HL from '../../public/articlesSVG/211924h-l.svg';
 import Icon211926HR from '../../public/articlesSVG/211926h-r.svg';
 import Icon211926HL from '../../public/articlesSVG/211926h-l.svg';
+import Icon211928HR from '../../public/articlesSVG/211928h-r.svg';
+import Icon211928HL from '../../public/articlesSVG/211928h-l.svg';
 
 export const getDafaultVerticalLockIcon = (
   fSet: IFSet
@@ -30,8 +32,8 @@ export const getDafaultVerticalLockIcon = (
     fSet.sideOfHinge === 'right'
   )
     return Icon211926HR;
-  if (article === '211928' && fSet.sideOfHinge === 'left') return Icon211924HL;
-  if (article === '211928' && fSet.sideOfHinge === 'right') return Icon211924HR;
+  if (article === '211928' && fSet.sideOfHinge === 'left') return Icon211928HL;
+  if (article === '211928' && fSet.sideOfHinge === 'right') return Icon211928HR;
 
   return null;
 };
